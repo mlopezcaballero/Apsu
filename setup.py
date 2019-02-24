3aa742c20b16737d2d7c32e039d1eafa98c56e82
MIGUEL_LOPEZ <miloc@MIGUELs-MacBook-Air.local>
2019-02-24 20:12:47 +0100

    Start of the project.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Aguathon: Modelizar el comportamiento del río Ebro para predecir posibles riadas e inundaciones.',
+    author='Miguel Lopez-Caballero',
+    license='MIT',
+)
